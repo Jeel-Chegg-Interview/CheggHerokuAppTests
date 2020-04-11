b160bb2586cc8b3453d8e036479d719e2b57d974
Jeel Patel <jeel.patel@verizonwireless.com>
2020-04-11 13:15:03 -0400

    Added code for first test and updated location of chromedriver

--- wikiTests/loginTests/TestLoginURLRedirect.java
+++ wikiTests/loginTests/TestLoginURLRedirect.java
@@ -1,13 +1,20 @@
 package loginTests;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import initialSettings.BrowserSetting;
-import objectRepository.LoginPage;
-import variablesData.UserVariables;
+import variablesData.UrlVariables;
 
+/**
+ * This class tests the URL redirect mechanism against expected
+ * @author Jeel Patel
+ * @version 1.0
+ *
+ */
 public class TestLoginURLRedirect {
 	BrowserSetting brSet;
 	WebDriver driver;
@@ -20,11 +27,24 @@ public class TestLoginURLRedirect {
 	}
 	@Test
 	public void loginURLRedirect() {
-
-		LoginPage lp = new LoginPage(driver);
-		lp.UsernameElement().sendKeys(UserVariables.USER_EMAIL);
-		lp.PasswordElement().sendKeys(UserVariables.USER_PASSWORD);
-		lp.Login_buttonElement().click();
+		String baseURL = UrlVariables.BASE_URL;
+		System.out.println("Base URL: "+baseURL);
 		
+		//navigating to https://chegg-qa-challenge.herokuapp.com/testuser-je8ds0dfd
+		driver.get(baseURL);
+		
+		//getting current URL (expecting redirect by now)
+		String currentURL = driver.getCurrentUrl();
+		System.out.println("Current URL: "+currentURL);
+		
+		String expectedURL = "https://chegg-qa-challenge.herokuapp.com/login";
+		
+		//Checking if the URL got redirected to expected, if so then the test will PASS
+		Assert.assertEquals(currentURL, expectedURL);
 		}
+	
+	@AfterClass
+	public void driverClosure() {
+		driver.close();
+	}
 }
